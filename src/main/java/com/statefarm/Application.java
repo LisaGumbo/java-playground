1eb0858da8538f2f23e7e0b6d2bd53ad7d426cd1
LisaGumbo <Lisa.Gumbo.ee83@statefarm.com>
2021-08-17 10:48:18 -0500

    adding name and email formatter

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -3,7 +3,7 @@ package com.statefarm;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println(args[0]+" <"+args[1]+">");
     }
 
 }
